34ac75fb1b25f55dfab5da6c76169a7e6527b835
yuhonghao0707 <1448856087@qq.com>
2019-10-17 16:50:25 +0800

    add Demo1

---
+++ src/main/java/com/interfacetest/Demo1.java
@@ -0,0 +1,9 @@
+package com.interfacetest;
+
+public abstract class Demo1 {
+    abstract void doit();
+
+    public static void main(String[] args) {
+        /*Demo1 d=new Demo1() ;*///不可以实例化
+    }
+}
