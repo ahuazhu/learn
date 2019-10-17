7efaa1320d30a6db6c6c466ebf4460b317a00fae
yuhonghao0707 <1448856087@qq.com>
2019-10-17 16:26:00 +0800

    QuadrangleUseInterface

---
+++ src/main/java/com/interfacetest/QuadrangleUseInterface.java
@@ -0,0 +1,37 @@
+package com.interfacetest;
+interface drawTest{
+    public void draw();
+}
+class ParallelogramgleUseInterface extends QuadrangleUseInterface implements drawTest{
+    public void draw(){
+        System.out.println("平行四边形.draw()");
+    }
+    void doAnyThing(){
+
+    }
+}
+class SquareUseInterface extends QuadrangleUseInterface implements drawTest{
+    public void draw(){
+        System.out.println("正方形.draw()");
+    }
+    void doAnyThing(){
+
+    }
+}
+class AnyThingUseInterface extends QuadrangleUseInterface{
+    void doAnything(){
+
+    }
+}
+public class QuadrangleUseInterface {
+    public void doAnyTthing(){
+
+    }
+
+    public static void main(String[] args) {
+        drawTest[] d={new SquareUseInterface(),new ParallelogramgleUseInterface()};
+        for (int i = 0; i < d.length; i++) {
+            d[i].draw();
+        }
+    }
+}
