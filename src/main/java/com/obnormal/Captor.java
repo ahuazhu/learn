9c7ff80cae85cf28d68af80972c3bb5355bb256c
yuhonghao0707 <1448856087@qq.com>
2019-10-18 18:45:00 +0800

    add Captor

---
+++ src/main/java/com/obnormal/Captor.java
@@ -0,0 +1,22 @@
+package com.obnormal;
+
+public class Captor {
+    static int quotient(int x,int y)throws MyException{
+        if (y<0){
+            throw new MyException("除数不能为负数");
+        }
+        return x/y;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int result=quotient(3,-1);
+        }catch (MyException e){
+            System.out.println(e.getMessage());
+        }catch (ArithmeticException e){
+            System.out.println("除数不能为0");
+        }catch (Exception e){
+            System.out.println("程序发生了其他的异常");
+        }
+    }
+}
