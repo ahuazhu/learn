3cb96c7c2578cdd9ec385809e43bbc129baa6d59
yuhonghao0707 <1448856087@qq.com>
2019-10-18 16:20:01 +0800

    InterfaceInner

---
+++ src/main/java/com/interfacetest/InterfaceInner.java
@@ -0,0 +1,24 @@
+package com.interfacetest;
+interface OutInterface{
+    public void f();
+}
+public class InterfaceInner {
+    public static void main(String[] args) {
+        OuterClass2 out=new OuterClass2();
+        OutInterface outinter=out.doit();
+        outinter.f();
+    }
+}
+class OuterClass2{
+    private class InnerClass implements OutInterface{
+        InnerClass(String s){
+            System.out.println(s);
+        }
+        public void f(){
+            System.out.println("访问内部类的f()方法");
+        }
+    }
+    public OutInterface doit(){
+        return new InnerClass("访问内部类构造方法");
+    }
+}
