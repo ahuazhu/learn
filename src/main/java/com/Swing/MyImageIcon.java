255aca36778480d8162ddb2185015321c6944d9c
yuhonghao0707 <1448856087@qq.com>
2019-10-19 17:13:43 +0800

    add MyImageIcon

---
+++ src/main/java/com/Swing/MyImageIcon.java
@@ -0,0 +1,25 @@
+package com.Swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class MyImageIcon extends JFrame {
+    public MyImageIcon(){
+        Container container=getContentPane();
+        JLabel jl=new JLabel("这是一个JFrame窗体",JLabel.CENTER);
+        URL url=MyImageIcon.class.getResource("imageButton.jpg");
+        Icon icon=new ImageIcon(url);
+        jl.setIcon(icon);
+        jl.setHorizontalAlignment(SwingConstants.CENTER);
+        jl.setOpaque(true);
+        container.add(jl);
+        setSize(250,100);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        new MyImageIcon();
+    }
+}
