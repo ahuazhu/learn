e99d46f75f97c6f38a3232f77bf58b18e4db1b02
yuhonghao0707 <1448856087@qq.com>
2019-10-20 19:14:33 +0800

    update FileTest

--- src/main/java/com/io/FileTest.java
+++ src/main/java/com/io/FileTest.java
@@ -6,15 +6,14 @@ public class FileTest {
     public static void main(String[] args) {
         File file=new File("word.txt");
         if(file.exists()){
-            file.delete();
-            System.out.println("文件已删除");
+            String name=file.getName();
+            long length=file.length();
+            boolean hidden=file.isHidden();
+            System.out.println("文件名称"+name);
+            System.out.println("文件长度"+length);
+            System.out.println("是否为隐藏文件"+hidden);
         }else{
-            try {
-                file.createNewFile();
-                System.out.println("文件已创建");
-            }catch (Exception e){
-                e.printStackTrace();
-            }
+                System.out.println("该文件不存在");
         }
     }
 }
