ea0eecb75ca44eb9017e3a49c0a50ed341304ebd
yuhonghao0707 <1448856087@qq.com>
2019-10-20 17:07:22 +0800

    add UpdateTest

---
+++ src/main/java/com/Object/UpdateTest.java
@@ -0,0 +1,24 @@
+package com.Object;
+
+import java.util.*;
+
+public class UpdateTest {
+    public static void main(String[] args) {
+        Map<String,String> map=new HashMap<String, String>();
+        map.put("01","李同学");
+        map.put("02","魏同学");
+        Set<String> set=map.keySet();
+        Iterator<String> it=set.iterator();
+        System.out.println("key集合中的元素");
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+        Collection<String> coll=map.values();
+        it=coll.iterator();
+        System.out.println("values集合中的元素");
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+
+    }
+}
