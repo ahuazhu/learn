188026345b91f2cfb87fb8c74462e3d353c322be
yuhonghao0707 <1448856087@qq.com>
2019-10-21 19:40:56 +0800

    add Demo1 and Demo2

---
+++ src/main/java/com/reflect/Demo1.java
@@ -0,0 +1,34 @@
+package com.reflect;
+
+import java.lang.reflect.Array;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Test test=new Test();
+        test.print();
+        test.is=(int[]) addArrayLength(test.is,10);
+        test.ss=(String[]) addArrayLength(test.ss,10);
+        test.print();
+    }
+    public static Object addArrayLength(Object array,int newLength){
+        Object newArray=null;
+        Class componentType=array.getClass().getComponentType();
+        newArray= Array.newInstance(componentType, newLength);
+        System.arraycopy(array, 0, newArray, 0, Array.getLength(array));
+        return newArray;
+    }
+}
+class Test {
+    public int[] is={1,2,3};
+    public String[] ss={"A","B","C"};
+    public void print(){
+        for (int i = 0; i < is.length; i++) {
+            System.out.println("is[" + i + "]=" + is[i]);
+        }
+        System.out.println();
+        for (int j = 0; j < ss.length; j++) {
+            System.out.println("is[" + j + "]=" + is[j]);
+        }
+        System.out.println();
+    }
+}
